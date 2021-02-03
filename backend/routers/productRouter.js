b00c6290771a69445dd1e439d6c18fde1f33a659
AhmedAGM <contact@agmtech.net>
2021-02-03 15:41:44 +0100

    commit

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -2,7 +2,6 @@ import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import data from '../data.js';
 import Product from '../models/productModel.js';
-
 import { isAdmin, isAuth, isSellerOrAdmin } from '../utils.js';
 
 const productRouter = express.Router();
@@ -13,14 +12,38 @@ productRouter.get(
     const category = req.query.category || '';
     const name = req.query.name || '';
     const seller = req.query.seller || '';
+    const order = req.query.order || '';
+    const min =
+      req.query.min && Number(req.query.min) !== 0 ? Number(req.query.min) : 0;
+    const max =
+      req.query.max && Number(req.query.max) !== 0 ? Number(req.query.max) : 0;
+    const rating =
+      req.query.rating && Number(req.query.rating) !== 0
+        ? Number(req.query.rating)
+        : 0;
     const nameFilter = name ? { name: { $regex: name, $options: 'i' } } : {};
     const sellerFilter = seller ? { seller } : {};
     const categoryFilter = category ? { category } : {};
+    const priceFilter = min && max ? { price: { $gte: min, $lte: max } } : {};
+    const ratingFilter = rating ? { rating: { $gte: rating } } : {};
+    const sortOrder =
+      order === 'lowest'
+        ? { price: 1 }
+        : order === 'highest'
+        ? { price: -1 }
+        : order === 'toprated'
+        ? { rating: -1 }
+        : { _id: -1 };
+
     const products = await Product.find({
       ...sellerFilter,
       ...nameFilter,
       ...categoryFilter,
-    }).populate('seller', 'seller.name seller.logo');
+      ...priceFilter,
+      ...ratingFilter,
+    })
+      .populate('seller', 'seller.name seller.logo')
+      .sort(sortOrder);
     res.send(products);
   })
 );
