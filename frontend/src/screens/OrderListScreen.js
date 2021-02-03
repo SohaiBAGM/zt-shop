69ca635c49be0e5518256e06dc3fff7e8f36e52c
AhmedAGM <contact@agmtech.net>
2021-02-03 10:59:40 +0100

    Commit Seller VIEW

--- frontend/src/screens/OrderListScreen.js
+++ frontend/src/screens/OrderListScreen.js
@@ -7,6 +7,7 @@ import { ORDER_DELETE_RESET } from '../constants/orderConstants';
 
 export default function OrderListScreen(props) {
   const orderList = useSelector((state) => state.orderList);
+  const sellerMode = props.match.path.indexOf('/seller') >= 0;
   const { loading, error, orders } = orderList;
   const orderDelete = useSelector((state) => state.orderDelete);
   const {
@@ -14,11 +15,14 @@ export default function OrderListScreen(props) {
     error: errorDelete,
     success: successDelete,
   } = orderDelete;
+
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch({ type: ORDER_DELETE_RESET });
-    dispatch(listOrders());
-  }, [dispatch, successDelete]);
+    dispatch(listOrders({ seller: sellerMode ? userInfo._id : '' }));
+  }, [dispatch, sellerMode, successDelete, userInfo._id]);
   const deleteHandler = (order) => {
     if (window.confirm('Are you sure to delete?')) {
       dispatch(deleteOrder(order._id));
